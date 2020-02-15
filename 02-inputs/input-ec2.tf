bb2339b9a42884edfdd6dc435a5e4d9eacbcd191
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-15 23:39:29 +0530

    Create input-ec2.tf

---
+++ 02-inputs/input-ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = "us-east-1"
+  access_key = "AKIATTF66BRW65BTQGBW"
+  secret_key = "+MEFG4QQ6/OaohVm8RsUUsNzSK6nRoTQuClQ51IR"
+}
+variable count_instances {
+}
+
+resource "aws_instance" "example" {
+  instance_type = "t2.micro"
+  ami           = "ami-00fa174a967f9222e"
+  count         = "var.count_instances"
+}
