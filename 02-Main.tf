7e0a5c9b3a62efc8bf616c69e7a0c8cf66fc51c2
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-18 03:15:28 +0530

    Update 02-Main.tf

--- 02-Main.tf
+++ 02-Main.tf
@@ -1,17 +1,17 @@
 resource "aws_vpc" "devops_vpc" {
-  cidr_block             = "${var.vpc_cidr}"
-  instance_tenancy       = "default"
+  cidr_block = "${var.vpc_cidr}"
+  instance_tenancy = "default"
 
-  tags                   = {
-    Name                 = "Devops_Vpc"
+  tags = {
+    Name = "Devops_Vpc"
   }
 }
-resource "aws_subnet" "devops_subnet-1" {
-  availability_zone       = "length(${var.AZ})"
-  vpc_id                  = "${aws_vpc.devops_vpc.id}"
-  cidr_block              = "${var.subnet_cidr}"
+resource "aws_subnet" "devops_subnets" {
+  count = "${length(var.AZ)}"
+  vpc_id = "${aws_vpc.devops_vpc.id}"
+  cidr_block = "${var.subnet_cidr}"
 
-  tags                    = {
-    Name                  = "Devops_Subnet-1"
+  tags = {
+    Name = "Devops_Subnet-1"
   }
 }
