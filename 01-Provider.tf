cf662c3492123996c00e4626adc6e5a5b276dd32
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-18 00:41:35 +0530

    Update 01-Provider.tf

--- 01-Provider.tf
+++ 01-Provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region  = "us-east-1"
+  region                = "us-east-1"
+  AWS_ACCESS_KEY_ID     = "AKIATTF66BRW65BTQGBW"
+  AWS_SECRET_ACCESS_KEY = "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"
 }
