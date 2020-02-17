e51a572c830952be421358999d0efa37820a202e
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-18 03:44:17 +0530

    Update 03-Vars.tf

--- 03-Vars.tf
+++ 03-Vars.tf
@@ -12,7 +12,11 @@ variable "vpc_cidr" {
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
-variable "AZ" {
-    type = list
-    default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+# variable "AZ" {
+#     type = list
+#     default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+# }
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
