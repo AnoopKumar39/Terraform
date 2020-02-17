59b5572cb2d9138f26d98cbf3a29c17721518a9b
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-18 00:33:22 +0530

    Create 02-Main.tf

---
+++ 02-Main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "devops_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Devops_Vpc"
+  }
+}
+resource "aws_subnet" "devops_subnet-1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Devops_Subnet-1"
+  }
+}
