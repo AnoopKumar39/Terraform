4f9be49fad5168d6a943104541d2a16404c7bab5
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-17 08:33:18 +0530

    Update main.tf

--- Modules/main.tf
+++ Modules/main.tf
@@ -1,7 +1,18 @@
-resource "aws_vpc" "devops" {
+resource "aws_vpc" "devops_vpc" {
   cidr_block       = "10.0.0.0/16"
 
   tags = {
+    Name       = "studentapp"
+    Created by = "terraform"
+  }
+}
+
+resource "aws_subnet" "devops_subnet" {
+  vpc_id     = "${aws_vpc.devops_vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
     Name = "studentapp"
+    Created by = "terraform"
   }
 }
