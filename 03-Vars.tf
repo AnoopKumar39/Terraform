c6ce438cd99f86fae24dddc9623fdf014165f3e6
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-18 01:14:38 +0530

    Update 03-Vars.tf

--- 03-Vars.tf
+++ 03-Vars.tf
@@ -2,3 +2,15 @@ variable "vpc_region" {
   type = string
   default = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+ variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "AZ" {
+    default = "us-east-1d"
+}
