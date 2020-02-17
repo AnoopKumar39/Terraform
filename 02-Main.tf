78e3ea026aeadf1b752556bb5a2cda0adcefb749
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-18 00:48:57 +0530

    Update 02-Main.tf

--- 02-Main.tf
+++ 02-Main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "devops_vpc" {
   }
 }
 resource "aws_subnet" "devops_subnet-1" {
-  vpc_id     = "{aws_vpc.devops_vpc.id}"
+  vpc_id     = "${aws_vpc.devops_vpc.id}"
   cidr_block = "10.0.1.0/24"
 
   tags = {
