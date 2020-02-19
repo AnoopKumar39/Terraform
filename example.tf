d1d1d4113cc40a1f382ff72ae5bfee4de398f341
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-20 04:47:38 +0530

    Update example.tf

--- example.tf
+++ example.tf
@@ -8,23 +8,27 @@ resource "aws_key_pair" "example" {
   public_key = file("~/.ssh/devops.pub")
 }
 
-resource "aws_instance" "example" {
-  key_name      = aws_key_pair.example.key_name
-  ami           = "ami-b374d5a5"
-  instance_type = "t2.micro"
+# resource "aws_instance" "example" {
+#   key_name      = aws_key_pair.example.key_name
+#   ami           = "ami-b374d5a5"
+#   instance_type = "t2.micro"
 
-  connection {
-  type        = "ssh"
-  user        = "ec2-user"
-  private_key = file("~/.ssh/devops")
-  host        = self.public_ip
-  }
+#   connection {
+#   type        = "ssh"
+#   user        = "ec2-user"
+#   private_key = file("~/.ssh/devops")
+#   host        = self.public_ip
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo amazon-linux-extras enable nginx1.12",
-      "sudo yum -y install nginx",
-      "sudo systemctl start nginx"
-    ]
-  }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo amazon-linux-extras enable nginx1.12",
+#       "sudo yum -y install nginx",
+#       "sudo systemctl start nginx"
+#     ]
+#   }
+# }
+
+output "aws_key_pair" {
+  value = aws_key_pair.key_name
 }
