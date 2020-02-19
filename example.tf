07e97c5591fc14bbf739e6340fcebc5da78f3e90
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-20 03:17:24 +0530

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,26 +1,14 @@
 provider "aws" {
   profile    = "default"
-  region     = "us-west-2"
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
-  # vpc_security_group_ids = ["sg-0077..."]
-  # subnet_id = "subnet-923a..."
-  depends_on = [aws_s3_bucket.example]
 }
 
-
-resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
-}
-
-resource "aws_s3_bucket" "example" {
-  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
-  # this name must be changed before applying this example to avoid naming
-  # conflicts.
-  bucket = "terraform-getting-started-guide39"
-  acl    = "private"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
