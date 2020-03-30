15a9ca5ac97de8c001c3fffab216e1ab12acc210
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-03-31 00:21:20 +0530

    Delete example.tf

--- example.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  profile    = "default"
-  region     = "${var.region}"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
-  instance_type = "t2.micro"
-}
