cff0aaaea43ab478be8638b5fa5697a396c30dd1
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-03-31 13:30:15 +0530

    Update vpc-subnet.tf

--- 02-data-sources/vpc-subnet.tf
+++ 02-data-sources/vpc-subnet.tf
@@ -25,15 +25,15 @@ output "subnets_list" {
     value = "${local.subnet_ids}"
 }
 
-#resource "aws_instance" "web" {
-#    ami           = "ami-0c322300a1dd5dc79"
-#    instance_type = "t2.micro"
-#    subnet_id     = "${element(local.subnet_ids,5)}"
-#}
+resource "aws_instance" "web" {
+    ami           = "ami-0c322300a1dd5dc79"
+    instance_type = "t2.micro"
+    subnet_id     = "${element(local.subnet_ids,random_integer.subnet_pick.result)}"
+}
 
 resource "random_integer" "subnet_pick" {
   min     = 1
-  max     = 100
+  max     = "${length(local.subnet_ids)}"
 }
 
 output "randon_number" {
