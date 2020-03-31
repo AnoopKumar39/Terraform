d4dc8e84f32fdec43dd97cc11478251470210a9a
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-03-31 12:48:56 +0530

    Update vpc-subnet.tf

--- 02-data-sources/vpc-subnet.tf
+++ 02-data-sources/vpc-subnet.tf
@@ -3,8 +3,16 @@ data "aws_vpcs" "demo" {
 
 locals {
     vpc_ids = "${tolist(data.aws_vpcs.demo.ids)}"
-    vpc_id  = "${element(local.vpc_ids,0)}"
+    vpc_id  = "${element(local.vpc_ids,1)}"
 }
 output "current_vpc" {
   value = "${local.vpc_id}"
+}
+
+data "aws_subnet_ids" "demo" {
+  vpc_id = "${local.vpc_id}"
+}
+
+output "all_subnets" {
+    value = "${data.aws_subnet_ids.demo.ids}"
 }
