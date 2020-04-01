e1272ff95a38f16364dd6c7f06fbd40fbe43c175
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-01 14:12:33 +0530

    Update modules.tf

--- 03-project/modules.tf
+++ 03-project/modules.tf
@@ -2,4 +2,5 @@ module "network" {
     source          = "./MODULES/vpc"
     VPC_CIDR        = "${var.PROJECT_CIDR}"
     VPC_NAME        = "${var.PROJECT_NAME}"
+    AZ              = "${data.aws_availability_zones.available.names}"
 }
