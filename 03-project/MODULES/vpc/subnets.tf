d1d264c57b90811f2f4bd0d829ce95f4d589a7d6
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-01 15:39:46 +0530

    Update subnets.tf

--- 03-project/MODULES/vpc/subnets.tf
+++ 03-project/MODULES/vpc/subnets.tf
@@ -1,10 +1,27 @@
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public_subnets" {
+  count                     = "${lenght(var.AZ)}"
   vpc_id                    = "${aws_vpc.main.id}"
-  cidr_block                = "10.0.1.0/24"
-  availability_zone         = "${element(var.AZ, 1)}"
+  cidr_block                = "${cidrsubnet(var.VPC_CIDR, 8, count.index)}"
+  availability_zone         = "${element(var.AZ, count.index)}"
 
 
   tags                      = {
-    Name                    = "Main"
+    Name                    = "${var.VPC_NAME}-Public-Subnet-${count.index+1}"
+    "Created by"            = "Terraform"
+    "Modified_Time"         = "${timestamp()}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count                     = "${lenght(var.AZ)}"
+  vpc_id                    = "${aws_vpc.main.id}"
+  cidr_block                = "${cidrsubnet(var.VPC_CIDR, 8, count.index+6)}"
+  availability_zone         = "${element(var.AZ, count.index)}"
+
+
+  tags                      = {
+    Name                    = "${var.VPC_NAME}-Private-Subnet-${count.index+1}"
+    "Created by"            = "Terraform"
+    "Modified_Time"         = "${timestamp()}"
   }
 }
