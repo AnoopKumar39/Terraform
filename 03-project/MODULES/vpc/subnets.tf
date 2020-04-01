c1b3c3f06bbcc5a21daf5bf5a48fdf1fd0edf5e9
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-01 14:12:35 +0530

    Update subnets.tf

--- 03-project/MODULES/vpc/subnets.tf
+++ 03-project/MODULES/vpc/subnets.tf
@@ -1,8 +1,10 @@
 resource "aws_subnet" "main" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.1.0/24"
+  vpc_id                    = "${aws_vpc.main.id}"
+  cidr_block                = "10.0.1.0/24"
+  availability_zone         = "${element(var.AZ, 6)}"
 
-  tags = {
-    Name = "Main"
+
+  tags                      = {
+    Name                    = "Main"
   }
 }
