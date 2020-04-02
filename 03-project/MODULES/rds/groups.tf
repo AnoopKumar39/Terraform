1fc5b0748c7a7ce0ae577c640a156aea31285a08
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-02 14:17:18 +0530

    Update groups.tf

--- 03-project/MODULES/rds/groups.tf
+++ 03-project/MODULES/rds/groups.tf
@@ -2,3 +2,13 @@ resource "aws_db_parameter_group" "default" {
   name          = "mariadb-10-3-pg"
   family        = "mariadb10.3"
 }
+
+resource "aws_db_subnet_group" "default" {
+  name          = "main"
+  subnet_ids    = "${var.PRIVATE_SUBNETS}"
+
+  tags = {
+    Name        = "${var.VPC_NAME}-Subnet-Group"
+  }
+}
+
