4f232e7bb43c523d70bbc43be6c5e336cf9692f0
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-02 14:17:21 +0530

    Update rds.tf

--- 03-project/MODULES/rds/rds.tf
+++ 03-project/MODULES/rds/rds.tf
@@ -8,4 +8,6 @@ resource "aws_db_instance" "studentapp-db" {
   username             = "student"
   password             = "student1"
   parameter_group_name = "${aws_db_parameter_group.default.id}"
+  identifier           = "${lower(var.VPC_NAME)}-db"
+  db_subnet_group_name = "${aws_db_subnet_group.default.id}"
 }
