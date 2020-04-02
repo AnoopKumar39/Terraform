53e84fc0f6dae27c88757445f42de6d8deb5cfff
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-02 14:56:54 +0530

    Update modules.tf

--- 03-project/modules.tf
+++ 03-project/modules.tf
@@ -7,8 +7,9 @@ module "network" {
 
 module "database" {
     source          = "./MODULES/rds"
-    #VPC_CIDR        = "${var.PROJECT_CIDR}"
+    VPC_CIDR        = "${var.PROJECT_CIDR}"
     VPC_NAME        = "${var.PROJECT_NAME}"
     #AZ              = "${data.aws_availability_zones.available.names}"
     PRIVATE_SUBNETS = "${module.network.PRIVATE_SUBNETS}"
+    VPC_ID        = "${module.network.VPC_ID}"
 }
