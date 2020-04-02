0f6cc084d2b3120e11e4bd32c6f7a089ca76d008
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-02 14:57:01 +0530

    Create sg.tf

---
+++ 03-project/MODULES/rds/sg.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "allow_mariadb" {
+  name        = "allow_mariadb"
+  description = "Allow MariaDB internal traffic"
+  vpc_id      = "${var.VPC_ID}"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["${var.VPC_CIDR}"]
+  }
+  
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
