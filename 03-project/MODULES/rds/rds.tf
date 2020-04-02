2b860a6d0ab3aae746b1600afc7aec6871284839
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-02 13:39:27 +0530

    Create rds.tf

---
+++ 03-project/MODULES/rds/rds.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "studentapp-db" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mariadb"
+  engine_version       = "10.3"
+  instance_class       = "db.t2.micro"
+  name                 = "studentapp"
+  username             = "student"
+  password             = "student1"
+  parameter_group_name = "${aws_db_parameter_group.default.id}"
+}
