fed45f4496a0884424b3b2b785616625324d9455
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-02 12:48:27 +0530

    Create routes.tf

---
+++ 03-project/MODULES/vpc/routes.tf
@@ -0,0 +1,15 @@
+resource "aws_route_table" "public-rt" {
+  vpc_id                = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block          = "${var.VPC_CIDR}"
+    gateway_id          = "${aws_internet_gateway.igw.id}"
+  }
+
+
+resource "aws_route_table_association" "public-rt" {
+  count           = "${length(var.AZ)}"
+  subnet_id       = "${element(aws_subnet.public_subnets.*.id, count.index)}"
+  route_table_id  = "${aws_route_table.public-rt.id}"
+}
+
