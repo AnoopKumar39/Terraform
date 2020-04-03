a4604ac80f8aca7b39718c9efeae20c6e230d136
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-04-03 12:04:10 +0530

    Update asg.tf

--- 03-project/MODULES/infrastructure/asg.tf
+++ 03-project/MODULES/infrastructure/asg.tf
@@ -3,6 +3,14 @@ resource "aws_placement_group" "application" {
   strategy = "spread"
 }
 
+
+resource "aws_launch_configuration" "application-config" {
+  name_prefix   = "terraform-lc-example"
+  image_id      = "ami-00fa174a967f9222e"
+  instance_type = "${var.INSTANCE_TYPE}"
+}
+
+
 resource "aws_autoscaling_group" "application" {
   name                      = "Studentapp-Asg"
   max_size                  = 5
@@ -12,8 +20,8 @@ resource "aws_autoscaling_group" "application" {
   desired_capacity          = 1
   force_delete              = true
   placement_group           = "${aws_placement_group.application.id}"
-  launch_configuration      = "${aws_launch_configuration.foobar.name}"
-  vpc_zone_identifier       = ["${aws_subnet.example1.id}", "${aws_subnet.example2.id}"]
+  launch_configuration      = "${aws_launch_configuration.application-config.name}"
+  vpc_zone_identifier       = "${var.PUBLIC_SUBNETS}"
 
 
  
