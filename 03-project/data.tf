765874035fae3b2034f55424aa141e5ea08ae152
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-11-15 02:01:25 +0530

    Update data.tf

--- 03-project/data.tf
+++ 03-project/data.tf
@@ -5,3 +5,4 @@ data "aws_availability_zones" "available" {
 data "aws_caller_identity" "current" {}
 ############
 ######data "aws_caller_identity" "current" {}
+######data "aws_caller_identity" "current" {}
